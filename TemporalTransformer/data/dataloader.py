034544c922c6c5fb9463b031e3b5b6f44b4ef7ce
Edward Effendy <edward.effendy@yahoo.com>
2024-12-31 15:20:48 +0900

    try adding global pelvis context in temporal dataloader

--- TemporalTransformer/data/dataloader.py
+++ TemporalTransformer/data/dataloader.py
@@ -153,217 +153,103 @@ class MotionLoader(data.Dataset):
 
 
 
-    def create_body_repr(self, with_hand=False, global_rot_norm=True, 
-                         smplx_model_path=None):
-        print('[INFO] create motion clip imgs by {}...'.format(self.mode))
+    def create_body_repr(self, global_rot_norm=True, smplx_model_path=None):
+        """
+        Creates body representations specifically for the 'local_joints_3dv' mode.
 
-        smplx_model_male = smplx.create(smplx_model_path, model_type='smplx', gender='male', ext='npz',
-                                        use_pca=False,  flat_hand_mean=True, # different here, true: mean hand pose is a flat hand
-                                        create_global_orient=True, create_body_pose=True, create_betas=True,
-                                        create_left_hand_pose=True, create_right_hand_pose=True, create_expression=True,
-                                        create_jaw_pose=True, create_leye_pose=True, create_reye_pose=True, create_transl=True,
-                                        batch_size=self.clip_len).to(device)
-        smplx_model_female = smplx.create(smplx_model_path, model_type='smplx', gender='female', ext='npz',
-                                          use_pca=False, flat_hand_mean=True, # different here, true: mean hand pose is a flat hand
-                                          create_global_orient=True, create_body_pose=True, create_betas=True,
-                                          create_left_hand_pose=True, create_right_hand_pose=True, create_expression=True,
-                                          create_jaw_pose=True, create_leye_pose=True, create_reye_pose=True, create_transl=True,
-                                          batch_size=self.clip_len).to(device)
+        Args:
+            global_rot_norm (bool): Whether to normalize the global rotation to face the y-axis.
+            smplx_model_path (str): Path to the SMPL-X model.
+        """
+        print('[INFO] Creating motion clip images for local_joints_3dv...')
+
+        # Load SMPL-X models
+        smplx_model_male = smplx.create(
+            smplx_model_path, model_type='smplx', gender='male', ext='npz',
+            use_pca=False, flat_hand_mean=True, create_global_orient=True, 
+            create_body_pose=True, create_betas=True, create_left_hand_pose=True,
+            create_right_hand_pose=True, create_expression=True, create_jaw_pose=True,
+            create_leye_pose=True, create_reye_pose=True, create_transl=True,
+            batch_size=self.clip_len
+        ).to(device)
+        
+        smplx_model_female = smplx.create(
+            smplx_model_path, model_type='smplx', gender='female', ext='npz',
+            use_pca=False, flat_hand_mean=True, create_global_orient=True,
+            create_body_pose=True, create_betas=True, create_left_hand_pose=True,
+            create_right_hand_pose=True, create_expression=True, create_jaw_pose=True,
+            create_leye_pose=True, create_reye_pose=True, create_transl=True,
+            batch_size=self.clip_len
+        ).to(device)
+
+        # Initialize a list to store processed clip images
         self.clip_img_list = []
+
         for i in tqdm(range(self.n_samples)):
-            ####################### set smplx params (gpu tensor) for each motion clip ##################
-            body_param_ = {}
-            body_param_['transl'] = self.data_dict_list[i]['trans']  # [T, 3]
-            body_param_['global_orient'] = self.data_dict_list[i]['poses'][:, 0:3]
-            body_param_['body_pose'] = self.data_dict_list[i]['poses'][:, 3:66]  # [T, 63]
-            body_param_['left_hand_pose'] = self.data_dict_list[i]['poses'][:, 66:111]  # [T, 45]
-            body_param_['right_hand_pose'] = self.data_dict_list[i]['poses'][:, 111:]  # [T, 45]
-            body_param_['betas'] = np.tile(self.data_dict_list[i]['betas'][0:10], (len(body_param_['transl']), 1))  # [T, 10]
+            # Set SMPL-X parameters
+            body_param_ = {
+                'transl': torch.from_numpy(self.data_dict_list[i]['trans']).float().to(device),
+                'global_orient': torch.from_numpy(self.data_dict_list[i]['poses'][:, :3]).float().to(device),
+                'body_pose': torch.from_numpy(self.data_dict_list[i]['poses'][:, 3:66]).float().to(device),
+                'betas': torch.from_numpy(
+                    np.tile(self.data_dict_list[i]['betas'][:10], (self.clip_len, 1))
+                ).float().to(device),
+            }
 
-            for param_name in body_param_:
-                body_param_[param_name] = torch.from_numpy(body_param_[param_name]).float().to(device)
+            # Normalize global translation/orientation (if enabled)
+            if global_rot_norm:
+                body_param_['transl'] -= body_param_['transl'][0]  # Center pelvis at origin
+                body_param_['transl'][:, 1] += 0.4  # Adjust height for visualization
 
+            # Generate joints using SMPL-X
+            smplx_model = smplx_model_male if self.data_dict_list[i]['gender'] == 'male' else smplx_model_female
+            with torch.no_grad():
+                smplx_output = smplx_model(return_verts=False, return_joints=True, **body_param_)
+                joints = smplx_output.joints[:, :25, :]  # Extract the first 25 joints
 
-            ############################### normalize first frame transl/gloabl_orient #############################
-            if not global_rot_norm:
-                # move bodies s.t. pelvis of the first frame is at the origin
-                body_param_['transl'] = body_param_['transl'] - body_param_['transl'][0]  # [T, 3]
-                body_param_['transl'][:,1] = body_param_['transl'][:,1] + 0.4
+            # Normalize joints for local coordinates
+            joints_np = joints.detach().cpu().numpy()
+            joints_np[:, :, [1, 2]] = joints_np[:, :, [2, 1]]  # Swap y and z axes (x, z, y)
+            
 
-            bs = body_param_['transl'].shape[0]
+            # Align to floor and reference joint
+            joints_np[:, :, 1] -= joints_np[:, :, 1].min()  # Place on floor
+            
+            # Extract pelvis global translation before reducing to local motion
+            pelvis_global = joints_np[:-1, 0:1, :].copy()  # [T-1, 1, 3]
+            
+            reference = joints_np[:, 0] * np.array([1, 0, 1])  # Add reference joint
+            cur_body = np.concatenate([reference[:, np.newaxis], joints_np], axis=1)
 
-            ######################## set  body representations (global joints for body/hand) #####################
-            if self.mode in ['global_joints', 'local_joints', 'local_joints_3dv', 'local_joints_3dv_v1',
-                             'global_markers', 'local_markers', 'local_joints_3dv_4chan',
-                             'local_markers_3dv', 'local_markers_3dv_4chan']:
-                if self.data_dict_list[i]['gender'] == 'male':
-                    smplx_output = smplx_model_male(return_verts=True, **body_param_)  # generated human body mesh
-                elif self.data_dict_list[i]['gender'] == 'female':
-                    smplx_output = smplx_model_female(return_verts=True, **body_param_)  # generated human body mesh
-                joints = smplx_output.joints  # [T, 127, 3]
+            # Compute velocities and normalize forward direction
+            velocity = cur_body[1:, 0:1] - cur_body[:-1, 0:1]
+            cur_body[:, :, 0] -= cur_body[:, 0:1, 0]
+            cur_body[:, :, 2] -= cur_body[:, 0:1, 2]
 
-                if self.mode in ['global_markers', 'local_markers', 'local_markers_3dv', 'local_markers_3dv_4chan']:
-                    markers = smplx_output.vertices[:, self.markers_ids, :]
+            # Filter forward direction
+            across = joints_np[:, 2] - joints_np[:, 1]
+            across /= np.linalg.norm(across, axis=-1, keepdims=True)
+            forward = np.cross(across, np.array([[0, 1, 0]]))
+            forward = filters.gaussian_filter1d(forward, 20, axis=0, mode='nearest')
+            forward /= np.linalg.norm(forward, axis=-1, keepdims=True)
 
+            # Remove Y rotation
+            target = np.array([[0, 0, 1]]).repeat(len(forward), axis=0)
+            rotation = Quaternions.between(forward, target)[:, np.newaxis]
+            cur_body = rotation * cur_body
 
-                if global_rot_norm:
-                    ##### transfrom to pelvis at origin, face y axis
-                    joints_frame0 = joints[0].detach()  # [N, 3] joints of first frame
-                    x_axis = joints_frame0[2, :] - joints_frame0[1, :]  # [3]
-                    x_axis[-1] = 0
-                    x_axis = x_axis / torch.norm(x_axis)
-                    z_axis = torch.tensor([0, 0, 1]).float().to(device)
-                    y_axis = torch.cross(z_axis, x_axis)
-                    y_axis = y_axis / torch.norm(y_axis)
-                    transf_rotmat = torch.stack([x_axis, y_axis, z_axis], dim=1)  # [3, 3]
-                    joints = torch.matmul(joints - joints_frame0[0], transf_rotmat)  # [T(/bs), 25, 3]
-                    if self.mode in ['global_markers', 'local_markers', 'local_markers_3dv', 'local_markers_3dv_4chan']:
-                        # markers_frame0 = markers[0].detach()
-                        # markers = torch.matmul(markers - markers_frame0[0], transf_rotmat)   # [T(/bs), n_marker, 3]
-                        markers = torch.matmul(markers - joints_frame0[0], transf_rotmat)   # [T(/bs), n_marker, 3]
+            # Adjust axes back and finalize
+            cur_body[:, :, [1, 2]] = cur_body[:, :, [2, 1]]
+            cur_body = cur_body[:-1, 1:, :]  # Remove the reference frame and final frame
+            
+            # Append pelvis global translation to cur_body
+            cur_body = np.concatenate([pelvis_global[:-1], cur_body], axis=1)  # [T-1, num_joints+1, 3]
 
-                ######## obtain binary contact labels
-                if self.mode in ['local_joints_3dv', 'local_joints_3dv_4chan']:
-                    # left_heel_id, right_heel_id = 7, 8, left_toe, right_toe = 10, 11
-                    # velocity criteria
-                    left_heel_vel = torch.norm((joints[1:, 7:8] - joints[0:-1, 7:8]) * self.clip_fps, dim=-1)  # [T-1, 1]
-                    right_heel_vel = torch.norm((joints[1:, 8:9] - joints[0:-1, 8:9]) * self.clip_fps, dim=-1)
-                    left_toe_vel = torch.norm((joints[1:, 10:11] - joints[0:-1, 10:11]) * self.clip_fps, dim=-1)
-                    right_toe_vel = torch.norm((joints[1:, 11:12] - joints[0:-1, 11:12]) * self.clip_fps, dim=-1)
-
-                    foot_joints_vel = torch.cat([left_heel_vel, right_heel_vel, left_toe_vel, right_toe_vel],
-                                                dim=-1)  # [T-1, 4]
-
-                    is_contact = torch.abs(foot_joints_vel) < 0.22  # todo: set contact threshold speed
-                    contact_lbls = torch.zeros([joints.shape[0], 4]).to(device)  # all -1, [T, 4]
-                    contact_lbls[0:-1, :][is_contact == True] = 1.0  # 0/1, 1 if contact for first T-1 frames
-
-                    # z height criteria
-                    z_thres = torch.min(joints[:, :, -1]) + 0.10   # todo: set contact threshold speed
-                    foot_joints = torch.cat([joints[:, 7:8], joints[:, 8:9], joints[:, 10:11], joints[:, 11:12]],
-                                            dim=-2)  # [T, 4, 3]
-                    thres_lbls = (foot_joints[:, :, 2] < z_thres).float()  # 0/1, [T, 4]
-
-                    # combine 2 criterias
-                    contact_lbls = contact_lbls * thres_lbls
-                    contact_lbls[-1, :] = thres_lbls[-1, :]  # last frame contact lbl: only look at z height
-                    # contact_lbls[contact_lbls == 0] = -1.0  # tensor, 1 for contact, -1 for no contact
-                    contact_lbls = contact_lbls.detach().cpu().numpy()
-
-                if self.mode in ['local_markers_3dv', 'local_markers_3dv_4chan']:
-                    # left foot markers: 25, 30, 16, right_foot_markers: 55, 60, 47
-                    # velocity criteria
-                    left_heel_vel = torch.norm((markers[1:, 9:10] - markers[0:-1, 9:10]) * self.clip_fps, dim=-1)  # [T-1, 1]
-                    right_heel_vel = torch.norm((markers[1:, 25:26] - markers[0:-1, 25:26]) * self.clip_fps, dim=-1)
-                    left_toe_vel = torch.norm((markers[1:, 40:41] - markers[0:-1, 40:41]) * self.clip_fps, dim=-1)
-                    right_toe_vel = torch.norm((markers[1:, 43:44] - markers[0:-1, 43:44]) * self.clip_fps, dim=-1)
-
-
-                    foot_markers_vel = torch.cat([left_heel_vel, right_heel_vel, left_toe_vel, right_toe_vel],
-                                                dim=-1)  # [T-1, 4]
-
-                    is_contact = torch.abs(foot_markers_vel) < 0.22  # todo: set contact threshold speed
-                    contact_lbls = torch.zeros([markers.shape[0], 4]).to(device)  # all -1, [T, 4]
-                    contact_lbls[0:-1, :][is_contact == True] = 1.0  # 0/1, 1 if contact for first T-1 frames
-
-                    # z height criteria
-                    z_thres = torch.min(markers[:, :, -1]) + 0.10  # todo: set contact threshold speed
-                    foot_markers = torch.cat([markers[:, 9:10], markers[:, 25:26], markers[:, 40:41], markers[:, 43:44]],
-                                             dim=-2)  # [T, 4, 3]
-                    thres_lbls = (foot_markers[:, :, 2] < z_thres).float()  # 0/1, [T, 4]
-
-                    # combine 2 criterias
-                    contact_lbls = contact_lbls * thres_lbls
-                    contact_lbls[-1, :] = thres_lbls[-1, :]  # last frame contact lbl: only look at z height
-                    contact_lbls = contact_lbls.detach().cpu().numpy()
-
-
-
-                ######## get body representation
-                body_joints = joints[:, 0:25]    # [T, 25, 3]  root(1) + body(21) + jaw/leye/reye(3)
-                hand_joints = joints[:, 25:55]   # [T, 30, 3]
-
-                # if not with_hand:
-                #     cur_body = body_joints  # [T, 25, 3]
-                # else:
-                #     cur_body = torch.cat([body_joints, hand_joints], axis=1)  # [T, 55, 3]
-
-
-                if self.mode in ['local_joints_3dv', 'local_joints_3dv_4chan']:
-                    cur_body = body_joints  # [T, 25, 3]
-                if self.mode in ['local_markers_3dv', 'local_markers_3dv_4chan']:
-                    cur_body = markers  # [T, n_marker, 3]
-
-                ############################# local joints from Holten ###############################
-                if self.mode in ['local_joints_3dv', 'local_joints_3dv_4chan',
-                                 'local_markers_3dv', 'local_markers_3dv_4chan']:
-                    cur_body = cur_body.detach().cpu().numpy()  # numpy, [T, 25 or 68, 3], in (x,y,z)
-
-                    cur_body[:, :, [1, 2]] = cur_body[:, :, [2, 1]] # swap y/z axis  --> in (x,z,y)
-
-                    """ Put on Floor """
-                    cur_body[:, :, 1] = cur_body[:, :, 1] - cur_body[:, :, 1].min()
-
-                    """ Add Reference Joint """
-                    reference = cur_body[:, 0] * np.array([1, 0, 1])  # [T, 3], (x,y,0)
-                    cur_body = np.concatenate([reference[:, np.newaxis], cur_body], axis=1)  # [T, 1+25 or 1+68, 3]
-
-                    """ Get Root Velocity in floor plane """
-                    velocity = (cur_body[1:, 0:1] - cur_body[0:-1, 0:1]).copy()  # [T-1, 3] ([:, 1]==0)
-
-                    """ To local coordinates """
-                    cur_body[:, :, 0] = cur_body[:, :, 0] - cur_body[:, 0:1, 0]  # [T, 1+25 or 1+68, 3]
-                    cur_body[:, :, 2] = cur_body[:, :, 2] - cur_body[:, 0:1, 2]
-
-                    """ Get Forward Direction """
-                    # using joints
-                    joints_np = joints.detach().cpu().numpy()
-                    joints_np[:, :, [1, 2]] = joints_np[:, :, [2, 1]] # swap y/z axis
-                    across = joints_np[:, 2] - joints_np[:, 1]
-                    across = across / np.sqrt((across ** 2).sum(axis=-1))[..., np.newaxis]
-                    # ipdb.set_trace()
-
-                    direction_filterwidth = 20
-                    forward = np.cross(across, np.array([[0, 1, 0]]))
-                    forward = filters.gaussian_filter1d(forward, direction_filterwidth, axis=0, mode='nearest')
-                    forward = forward / np.sqrt((forward ** 2).sum(axis=-1))[..., np.newaxis]
-
-                    """ Remove Y Rotation """
-                    target = np.array([[0, 0, 1]]).repeat(len(forward), axis=0)
-                    rotation = Quaternions.between(forward, target)[:, np.newaxis]
-                    cur_body = rotation * cur_body  # [T, 1+25 or 1+68, 3]
-
-                    """ Get Root Rotation """
-                    velocity = rotation[1:] * velocity  # [T-1, 1, 3]
-                    rvelocity = Pivots.from_quaternions(rotation[1:] * -rotation[:-1]).ps   # [T-1, 1]
-
-
-                    cur_body[:, :, [1, 2]] = cur_body[:, :, [2, 1]]
-                    cur_body = cur_body[0:-1, 1:, :]  # [T-1, 25 or 68, 3]
-
-                    if self.mode in ['local_joints_3dv', 'local_markers_3dv']:
-                        # global vel (3) + local pose (25*3) + contact label (4)
-                        cur_body = cur_body
-
-                    elif self.mode in ['local_joints_3dv_4chan', 'local_markers_3dv_4chan']:
-                        channel_local = np.concatenate([cur_body, contact_lbls[0:-1]], axis=-1)[np.newaxis, :, :]  # [1, T-1, d=75(204)+4]
-                        T, d = channel_local.shape[1], channel_local.shape[-1]
-                        global_x, global_y = velocity[:, :, 0], velocity[:, :, 2]  # [T-1, 1]
-                        channel_global_x = np.repeat(global_x, d).reshape(1, T, d)  # [1, T-1, d]
-                        channel_global_y = np.repeat(global_y, d).reshape(1, T, d)  # [1, T-1, d]
-                        channel_global_r = np.repeat(rvelocity, d).reshape(1, T, d)  # [1, T-1, d]
-
-                        cur_body = np.concatenate([channel_local, channel_global_x, channel_global_y, channel_global_r], axis=0)  # [4, T-1, d]
-
-
-
-
+            # Append processed clip to list
             self.clip_img_list.append(cur_body)
 
-
-
-
-        self.clip_img_list = np.asarray(self.clip_img_list)  # [N, T-1, d] / [N, 4, T-1, d]
+        # Convert list to numpy array
+        self.clip_img_list = np.asarray(self.clip_img_list)
 
         if self.normalize:
             prefix = os.path.join(self.log_dir, 'statistics')
